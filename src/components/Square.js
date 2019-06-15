10a9aac74718a7fbb3ec2680814a1b3109aea62c
ha3 <h.ozdemir13@yandex.com>
2019-06-15 15:39:54 +0300

    Add Square component

---
+++ src/components/Square.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Square(props) {
+  const className = 'square' + (props.highlight ? ' highlight' : '');
+
+  return (
+    <button className={className} onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+export default Square;
