2be23d2023a19a05e3125b8c7e3f693147dc305e
ha3 <h.ozdemir13@yandex.com>
2019-07-03 14:29:28 +0300

    Add available_moves function

--- ai.py
+++ ai.py
@@ -1,7 +1,12 @@
+import random
+
+def available_moves(board):
+    return [i for i in range(len(board)) if board[i] == None]
+
 def find_best_move(squares, focus):
     if focus == None:
-      focus = 0;
+        focus = 0;
 
-    for i in range(len(squares[focus])):
-      if squares[focus][i] == None:
-        return focus, i;
+    moves = available_moves(squares[focus])
+    c = random.choice(moves)
+    return focus, c;
