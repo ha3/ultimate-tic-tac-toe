aa2bd2525d2b7d9cbf015d3ab0174af48802eb66
ha3 <h.ozdemir13@yandex.com>
2019-09-01 22:10:25 +0300

    Convert board datas to bits before requesting best move.

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS, cross_origin
+from math import inf
 
-from ai import *
+from ai import Game
 
 app = Flask(__name__)
 CORS(app)
+
+def mask(position, player):
+    offset = 8 - position
+
+    if player == 'X':
+        offset += 9
+
+    return 1 << offset
 
 @app.route('/')
 def hello_world():
@@ -14,12 +23,36 @@ def hello_world():
 def response():
     content = request.get_json()
 
-    allBoards = content['allBoards']
+    all_boards = content['allBoards']
     focus = content['focus']
+    move = content['move']
+    global_board = content['globalBoard']
+    local_move_count = content['localMoveCount']
 
-    board, coordinate = find_best_move(allBoards, focus)
+    bit_all_boards = [0] * 9
+    bit_global_board = 0
+    bit_ended_boards = 0
+    bit_full_boards = 0
 
-    jsonResponse = {'board': board, 'coordinate': coordinate}
+    for i in range(9):
+        for j in range(9):
+            bit_all_boards[i] = bit_all_boards[i] | mask(j, allBoards[i][j])
+
+        if global_board[i] in ['X', 'O', '-']:
+            bit_ended_boards = bit_ended_boards | (1 << i)
+
+            if global_board[i] != '-':
+                bit_global_board = bit_global_board | (1 << i)
+
+        if local_move_count[i] == 8:
+            bit_full_boards = bit_full_boards | (1 << i)
+
+
+    game = Game(all_boards=bit_all_boards, global_board=bit_global_board, ended_boards=bit_ended_boards,
+                full_boards=bit_full_boards, turn=-1, focus=focus, move=move+1)
+    best_move = game.alpha_beta((81-move_count), -inf, inf, focus)
+
+    jsonResponse = {'best_move': best_move}
     return jsonify(jsonResponse)
 
 if __name__ == '__main__':
